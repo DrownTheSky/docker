73bba35ccf2ecb4aae2a3d06c9435776b2e589b5
DrownTheSky <2676293258@qq.com>
2025-10-12 09:21:56 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,14 +7,13 @@ WORKSPACE="$(cd $PROJECT_DIR/.. && pwd)"
 if [[ -n "$1" && -d "$1" ]]; then
     WORKSPACE="$(realpath "$1")"
 fi
-mkdir -p $WORKSPACE/data
-
-docker build -t $DOCKER_IMAGE $PROJECT_DIR \
-    --build-arg UID=$(id -u) \
-    --build-arg GID=$(id -g) \
-    --build-arg UNAME=$USER
 
 if ! docker ps -a --format '{{.Names}}' | grep -qw $DOCKER_CONTAINER; then
+    mkdir -p $WORKSPACE/data
+    docker build -t $DOCKER_IMAGE $PROJECT_DIR \
+        --build-arg UID=$(id -u) \
+        --build-arg GID=$(id -g) \
+        --build-arg UNAME=$USER
     docker run --rm -it --gpus all --privileged --name $DOCKER_CONTAINER --network host \
         -v $WORKSPACE:/home/$USER/source \
         -v $PROJECT_DIR/.conan:/home/$USER/.conan \
